2cf92ad6e1c81420f5049558cea19cdad99b4a54
U-crown-royal\nwarden <nwarden@crown-royal.(none)>
2012-01-04 13:38:40 -0500

    first commit ever!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRails::Application.config.secret_token = 'fe5233f357eb4d7fe793bc362bc286cbcc3fbda3c964329d0455e5c8e0015034a31fba429907b86d50dc2694248a4b47ae333d10e88bcff15470902a9cd9cbbf'
